e06598c81d3918dadd855efed5171c20cd47f42d
Thiago <thiago_crestani@hotmail.com>
2018-11-14 21:34:04 -0200

    Trabalho Pronto

---
+++ config/config.conf.php
@@ -0,0 +1,12 @@
+<?php
+$conf = array(
+    'URL' => "http://localhost/video-aula-php-mvc/public/",
+    'HOST' => "127.0.0.1",
+    'PORT' => 3306,
+    'DB_NAME' => "singleton",
+    'USER_NAME' => "root",
+    'PASSWORD' => "root",
+    'DEFAULT_CONTROLLER' => "ControleMenu",
+    'DEFAULT_METHOD' => "Inicio",
+     'PATH_UPLOADS' => "./uploads/",
+);
